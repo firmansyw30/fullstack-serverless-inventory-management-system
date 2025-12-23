adb8152fd33610b56094e3e8ee7164958f85bcc3
firman_devops-at-392987323540 <firmansyah.wicaksono@mapid.co.id>
2025-12-23 14:19:48 +0700

    initial commit

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header({ onAddNewClick }) {
+  return (
+    <header className="header">
+      <h1>Simple Inventory Management System</h1>
+      <button className="add-button" onClick={onAddNewClick}>Add New Item</button>
+    </header>
+  );
+}
+
+export default Header;
