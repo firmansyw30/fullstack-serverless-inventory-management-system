b998aa15f0eaa6c78af523f39002de60833474a2
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2025-12-25 23:57:08 +0700

    feat : add google beta provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,4 +1,28 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
