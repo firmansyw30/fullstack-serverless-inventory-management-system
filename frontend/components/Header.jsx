6540d0a2f6e16fe24a2b71fc459f5ffeff884394
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2025-12-29 06:36:10 +0700

    experiment with home button but not clear

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import '../src/App.css';
 function Header({ onAddNewClick }) {
+  const handleHomeClick = () => {
+    console.log('Home button clicked');
+    window.location.href = '/';
+  };
   return (
     <header className="header">
-      <h1>Simple Inventory Management System</h1>
+      <button className="home-button" onClick={handleHomeClick}>
+        <h1>Simple Inventory Management System</h1>
+      </button>
       <button className="add-button" onClick={onAddNewClick}>Add New Item</button>
     </header>
   );
